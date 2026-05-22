c3a85b3475f0760f9abb619fd68546e817fe41d6
Shreyan Ghosh <shreyan.loyality@gmail.com>
2026-05-23 01:22:21 +0530

    Model Trainer Added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,8 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from data_transformation import DataTransformation
 from data_transformation import DataTransformationConfig
+from model_trainer import ModelTrainer
+from model_trainer import ModelTrainerConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -46,4 +48,9 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    obj2=ModelTrainer()
+    obj2.initiate_model_trainer(train_arr,test_arr)
+
+    
